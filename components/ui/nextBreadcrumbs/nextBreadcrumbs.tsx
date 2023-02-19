e5151730bd449f85c80fd7e55daca1f9aaa0b798
Mushraf <mushrafaltaf@gmail.com>
2023-02-19 13:00:28 +0530

    Add: breadcrumbs

---
+++ components/ui/nextBreadcrumbs/nextBreadcrumbs.tsx
@@ -0,0 +1,83 @@
+// MUI Imports
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+
+// NEXT Imports
+import { useRouter } from 'next/router';
+
+// Components Imports
+import GeneralContainer from '../generalContainer/generalContainer';
+
+export interface NextBreadcrumbsProps {
+  mapTo: {
+    from: string,
+    to: string
+  }
+}
+
+export default function NextBreadcrumbs(props: NextBreadcrumbsProps) {
+    const router = useRouter();
+
+    // To match [course] in Regex it needs to be escaped \[course\]
+    function escapeStringIfBracketsExist(str: string) {
+      if (/\[|\]/.test(str)) {
+        str = str.replace(/[[\]]/g, '\\$&');
+      }
+      return str;
+    }
+
+    return (
+      <GeneralContainer>
+        <Breadcrumbs separator="›" aria-label="breadcrumb">
+          {
+            router.asPath
+            .split('/')
+            .map((path, index, array) => {
+              if (router.route.split('/')[index] === props.mapTo.from) {
+                // Replace example: [course]/lessons with <path>/lessons. 
+                const newPath = props.mapTo.to.replace(new RegExp(escapeStringIfBracketsExist(router.route.split('/')[index]), 'g'), path);
+                const text = path;
+                const link = array.slice(0, index).join('/') + "/" + newPath;
+                const last = index + 1 == array.length;
+                return <Crumb text={text} link={link} last={last} key={text} />
+              }
+              if (path !== 'lessons') {
+                const text = path;
+                const link = array.slice(0, index + 1).join('/');
+                const last = index + 1 == array.length;
+                return <Crumb text={text} link={link} last={last} key={text} />
+              }
+            })
+          }
+        </Breadcrumbs>
+      </GeneralContainer>
+    );
+}
+
+interface CrumbProps {
+  text: string,
+  link: string,
+  last: boolean
+}
+
+function Crumb(props: CrumbProps) {
+
+  function capitalize(word: string): string {
+    return word
+      .toLowerCase()
+      .replace(/\w/, (firstLetter: string) => firstLetter.toUpperCase());
+  }
+
+  if (props.last) {
+    return <Typography color="text.primary">
+      {capitalize(props.text)}
+    </Typography>
+  }
+
+  return (
+    <Link underline="hover" color="inherit" href={props.link}>
+      {capitalize(props.text)}
+    </Link>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/ui/layout/layout';
 import NavBar from '../components/ui/navBar/navBar';
+import NextBreadcrumbs from '../components/ui/nextBreadcrumbs/nextBreadcrumbs';
 
 const navLinks = [
   {name: 'Home', link: '/'},
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return(
     <Layout>
       <NavBar pages={navLinks} />
+      <NextBreadcrumbs mapTo={{ from: '[course]', to: '[course]/lessons' }} />
       <Component {...pageProps} />
     </Layout>
   )
